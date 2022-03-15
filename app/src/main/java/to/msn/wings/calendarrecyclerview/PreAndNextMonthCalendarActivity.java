1f1a78b6945f34ac798cc74b977a716a6eb0119b
hayato-kame-new <guishan.meisha0330@gmail.com>
2022-03-15 09:39:21 +0900

    SubActivity change Fragment add

--- app/src/main/java/to/msn/wings/calendarrecyclerview/PreAndNextMonthCalendarActivity.java
+++ app/src/main/java/to/msn/wings/calendarrecyclerview/PreAndNextMonthCalendarActivity.java
@@ -1,16 +1,11 @@
 package to.msn.wings.calendarrecyclerview;
 
-import static to.msn.wings.calendarrecyclerview.R.color.pinkSunday;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,54 +15,60 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
 
 /**
- * メインアクティビティの上にCurrentMonthFragmentが乗っています
+ * この第二画面のアクティビティの上に、PreAndNextCalendarFragment
  */
-public class MainActivity extends AppCompatActivity {
+public class PreAndNextMonthCalendarActivity extends AppCompatActivity {
 
 //    private TextView titleText;
-//    private Button prevButton, nextButton;
+//    private Button prevButton, nextButton, currentMonthButton;
 //    private CalendarAdapter calendarAdapter;
 //    private RecyclerView recyclerView;
 //    DateManager dateManager;
 //
 //    private int SPAN_COUNT = 7;
 
-
-
-    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pre_and_next_month_calendar);
 
+
+
+      //   Intent intent = getIntent();
+
+//        //  null かどうかのチェックが必要です どっちのボタンから遷移してきたのか  どっちかは nullになるので
+//        Date prevButtonDate = (Date)intent.getSerializableExtra("prevButtonDate");
+//        //  null かどうかのチェックが必要です どっちのボタンから遷移してきたのか どっちかは nullになるので
+//        Date nextButtonDate = (Date)intent.getSerializableExtra("nextButtonDate");
 //
 //        dateManager = new DateManager();
-//        // getDays()で、現在の月の日にちオブジェクトを取得する 今月だと 引数なしのgetDays()　を呼び出す
-//        // 今月を取得する
-//        List<Date> dates =  dateManager.getDays();
-//        // countいらない？？？
-//       //  int count = dates.size();
 //
 //
-//        // タイトルのテキストを設定する
+//        List<Date> dates = null;
+//
+//
 //        titleText = findViewById(R.id.titleText);
-//        // 最初の土曜日は、その月に必ずなってるから
-//        Date firstSaturdayDate = dates.get(6);
-//        SimpleDateFormat format = new SimpleDateFormat("今月のカレンダー yyyy年 MM月");
-//        String title = format.format(firstSaturdayDate);
+//        SimpleDateFormat format = new SimpleDateFormat("yyyy年 MM月");
+//        // ここで条件分岐します
+//        String title = "";
+//        if (prevButtonDate != null) {
+//           title = format.format(prevButtonDate);
+//            dates = dateManager.getDays(prevButtonDate);  //引数ありのgetDays(Date date)　を呼び出す
+//
+//        } else if (nextButtonDate != null) {
+//            title = format.format(nextButtonDate);
+//            dates = dateManager.getDays(nextButtonDate); //引数ありのgetDays(Date date)　を呼び出す
+//
+//        }
 //        titleText.setText(title);
 //
-//        // フォーマットし直し
-//         format = new SimpleDateFormat("d");  // "dd" だと　　01  02 となってしまう
+//// フォーマットし直し
+//        format = new SimpleDateFormat("d");  // "dd" だと　　01  02 となってしまう
 //
-//        // 本日
-//        Calendar calendar = Calendar.getInstance();
-//        Date dateObjToday = calendar.getTime();
 //        /**
-//         * 表示だけのテキストのリスト
+//         * 表示だけのリスト
 //         */
 //        ArrayList<CalendarCellItem> data = new ArrayList<>();
 //        for (int i = 0; i < dates.size(); i++) {
@@ -79,21 +80,13 @@ public class MainActivity extends AppCompatActivity {
 //
 //            String display = format.format(date);
 //
-//             item.setDateText(display);  // セットします
+//            item.setDateText(display);  // セットします
 //
-//            // Dateクラスでは、compareToメソッドを使って日付の比較をします  compareToメソッドの戻り値は、
-//            // メソッドの呼び出し元の値が、引数と等しい場合は0
-//            // メソッドの呼び出し元の値が、引数より前の場合は-1
-//            // メソッドの呼び出し元の値が、引数より後の場合は1
-//            // 0 か 1 か　-1 を返すので if 使う時に 0だったらというふうに書くので 注意
-//            if (date.compareTo(dateObjToday) == 0) {
-//                item.setTextViewToday("●");
-//            }
-//
-//             data.add(item);
+//            data.add(item);
 //        }
 //
-//
+//        // 最初の土曜日は、その月に必ずなってるから
+//        Date firstSaturdayDate = dates.get(6);
 //
 //
 //        // 表示してる月よりも１つ前の月を表示するためのボタン
@@ -102,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
 //            @Override
 //            public void onClick(View v) {
 //                // MainActivityで今、表示をしている月の情報を取得する MainActivityでは、初期の画面 今月のカレンダーを表示するので
-//               //  アクティビティを新たに生成し、
+//                //  アクティビティを新たに生成し、
 //                //  新しいアクティビティにMainActivityの firstSaturdayDateの情報から、１ヶ月前にした情報を渡す
 //                // Date型の計算を行いたい場合には、Calendar型に一度変換し、計算を行います。
 //                Calendar calendar = Calendar.getInstance();
@@ -112,13 +105,11 @@ public class MainActivity extends AppCompatActivity {
 //                // これで1月前の最初の土曜日の日付が取得できている
 //                date = calendar.getTime();
 //
-//                Intent intent = new Intent(MainActivity.this, PreAndNextMonthCalendarActivity.class);
+//                Intent intent = new Intent(PreAndNextMonthCalendarActivity.this, PreAndNextMonthCalendarActivity.class);
 //
 //                intent.putExtra("prevButtonDate", date);  // 1月前の最初の土曜日の日付を送る Date型情報を渡します
 //                startActivity(intent);
 //
-//                Activity parentActivity = getActivity();  // 自分自身が所属するアクティビティを終了させます
-//                parentActivity.finish();
 //
 //            }
 //        });
@@ -135,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
 //                // これで1月先の最初の土曜日の日付が取得できている
 //                date = calendar.getTime();
 //
-//                Intent intent = new Intent(MainActivity.this, PreAndNextMonthCalendarActivity.class);
+//                Intent intent = new Intent(PreAndNextMonthCalendarActivity.this, PreAndNextMonthCalendarActivity.class);
 //
 //                intent.putExtra("nextButtonDate", date);  // 1月先の最初の土曜日の日付を送ってる Date型情報を渡します
 //                startActivity(intent);
@@ -144,8 +135,37 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 //
+//        //  今月の表示に戻る MainActivityに戻る
+//        currentMonthButton = findViewById(R.id.currentMonthButton);
+//        currentMonthButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // MainActivityで今、表示をしている月の情報を取得する MainActivityでは、初期の画面 今月のカレンダーを表示するので
+//                //  アクティビティを新たに生成し、
+//                //  新しいアクティビティにMainActivityの firstSaturdayDateの情報から、１ヶ月前にした情報を渡す
+//                // Date型の計算を行いたい場合には、Calendar型に一度変換し、計算を行います。
+////                Calendar calendar = Calendar.getInstance();
+////                calendar.setTime(firstSaturdayDate);
+////                calendar.add(Calendar.MONTH, -1);  // -1 をして ひと月前に
+////                Date date = new Date();
+////                // これで1月前の最初の土曜日の日付が取得できている
+////                date = calendar.getTime();
+//
+//                Intent intent = new Intent(PreAndNextMonthCalendarActivity.this, MainActivity.class);
+//
+//             //  intent.putExtra("prevButtonDate", date);  // 1月前の最初の土曜日の日付を送る Date型情報を渡します
+//                startActivity(intent);
 //
 //
+//
+//
+//            }
+//        });
+//
+//
+//
+//        // マニフェストファイルによって、activity_month_calendar.xmlとの紐付けができてるので、
+//        // idとかもこのまま使える メインレイアウトと同じだけど問題なく使える
 //        RecyclerView rv = findViewById(R.id.rv);
 //        rv.setHasFixedSize(true);  // パフォーマンス向上
 //
@@ -155,5 +175,6 @@ public class MainActivity extends AppCompatActivity {
 //
 //        RecyclerView.Adapter adapter = new CalendarAdapter(data);
 //        rv.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/to/msn/wings/calendarrecyclerview/PreAndNextMonthCalendarFragment.java
+++ app/src/main/java/to/msn/wings/calendarrecyclerview/PreAndNextMonthCalendarFragment.java
@@ -1,12 +1,16 @@
 package to.msn.wings.calendarrecyclerview;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,7 +20,8 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class MonthCalendarActivity extends AppCompatActivity {
+public class PreAndNextMonthCalendarFragment extends Fragment {
+
 
     private TextView titleText;
     private Button prevButton, nextButton, currentMonthButton;
@@ -26,17 +31,33 @@ public class MonthCalendarActivity extends AppCompatActivity {
 
     private int SPAN_COUNT = 7;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_month_calendar);
 
-        Intent intent = getIntent();
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       //  return inflater.inflate(R.layout.fragment_pre_and_next_month_calendar, container, false);
+
+        Activity parentActivity = getActivity();
+
+        View view = inflater.inflate(R.layout.fragment_pre_and_next_month_calendar, container, false);
+
+        Intent intent = parentActivity.getIntent();
+
+        Date prevButtonDate = null;
+        Date nextButtonDate = null;
+        Bundle extras = intent.getExtras();
+        if(extras != null) {
+            //  null かどうかのチェックが必要です どっちのボタンから遷移してきたのか  どっちかは nullになるので
+             prevButtonDate = (Date)intent.getSerializableExtra("prevButtonDate");  // nullが入ってるかもしれないです
+            //  null かどうかのチェックが必要です どっちのボタンから遷移してきたのか どっちかは nullになるので
+             nextButtonDate = (Date)intent.getSerializableExtra("nextButtonDate");  // nullが入ってるかもしれないです
+        }
 
         //  null かどうかのチェックが必要です どっちのボタンから遷移してきたのか  どっちかは nullになるので
-        Date prevButtonDate = (Date)intent.getSerializableExtra("prevButtonDate");
+   //     Date prevButtonDate = (Date)intent.getSerializableExtra("prevButtonDate");
         //  null かどうかのチェックが必要です どっちのボタンから遷移してきたのか どっちかは nullになるので
-        Date nextButtonDate = (Date)intent.getSerializableExtra("nextButtonDate");
+    //     Date nextButtonDate = (Date)intent.getSerializableExtra("nextButtonDate");
 
         dateManager = new DateManager();
 
@@ -44,12 +65,12 @@ public class MonthCalendarActivity extends AppCompatActivity {
         List<Date> dates = null;
 
 
-        titleText = findViewById(R.id.titleText);
+        titleText = view.findViewById(R.id.titleText);
         SimpleDateFormat format = new SimpleDateFormat("yyyy年 MM月");
         // ここで条件分岐します
         String title = "";
         if (prevButtonDate != null) {
-           title = format.format(prevButtonDate);
+            title = format.format(prevButtonDate);
             dates = dateManager.getDays(prevButtonDate);  //引数ありのgetDays(Date date)　を呼び出す
 
         } else if (nextButtonDate != null) {
@@ -85,7 +106,7 @@ public class MonthCalendarActivity extends AppCompatActivity {
 
 
         // 表示してる月よりも１つ前の月を表示するためのボタン
-        prevButton = findViewById(R.id.prevButton);
+        prevButton = view.findViewById(R.id.prevButton);
         prevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -100,7 +121,8 @@ public class MonthCalendarActivity extends AppCompatActivity {
                 // これで1月前の最初の土曜日の日付が取得できている
                 date = calendar.getTime();
 
-                Intent intent = new Intent(MonthCalendarActivity.this, MonthCalendarActivity.class);
+              //  Intent intent = new Intent(PreAndNextMonthCalendarActivity.this, PreAndNextMonthCalendarActivity.class);
+                Intent intent = new Intent(parentActivity, PreAndNextMonthCalendarActivity.class);
 
                 intent.putExtra("prevButtonDate", date);  // 1月前の最初の土曜日の日付を送る Date型情報を渡します
                 startActivity(intent);
@@ -110,7 +132,7 @@ public class MonthCalendarActivity extends AppCompatActivity {
         });
 
         // 次の月を表示するためのボタン
-        nextButton = findViewById(R.id.nextButton);
+        nextButton = view.findViewById(R.id.nextButton);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -121,7 +143,8 @@ public class MonthCalendarActivity extends AppCompatActivity {
                 // これで1月先の最初の土曜日の日付が取得できている
                 date = calendar.getTime();
 
-                Intent intent = new Intent(MonthCalendarActivity.this, MonthCalendarActivity.class);
+              //   Intent intent = new Intent(PreAndNextMonthCalendarActivity.this, PreAndNextMonthCalendarActivity.class);
+                Intent intent = new Intent(parentActivity, PreAndNextMonthCalendarActivity.class);
 
                 intent.putExtra("nextButtonDate", date);  // 1月先の最初の土曜日の日付を送ってる Date型情報を渡します
                 startActivity(intent);
@@ -130,8 +153,8 @@ public class MonthCalendarActivity extends AppCompatActivity {
             }
         });
 
-        //  今月の表示に戻る MainActivityに戻る
-        currentMonthButton = findViewById(R.id.currentMonthButton);
+        //  今月の表示に戻る MainActivityに戻る  自分自身が所属するアクティビティを終了させます
+        currentMonthButton = view.findViewById(R.id.currentMonthButton);
         currentMonthButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -146,11 +169,15 @@ public class MonthCalendarActivity extends AppCompatActivity {
 //                // これで1月前の最初の土曜日の日付が取得できている
 //                date = calendar.getTime();
 
-                Intent intent = new Intent(MonthCalendarActivity.this, MainActivity.class);
+              //  Intent intent = new Intent(PreAndNextMonthCalendarActivity.this, MainActivity.class);
+                Intent intent = new Intent(parentActivity, MainActivity.class);
 
-             //  intent.putExtra("prevButtonDate", date);  // 1月前の最初の土曜日の日付を送る Date型情報を渡します
+                //  intent.putExtra("prevButtonDate", date);  // 1月前の最初の土曜日の日付を送る Date型情報を渡します
                 startActivity(intent);
 
+                // 自分自身が所属するアクティビティを終了させます
+                Activity parentActivity = getActivity();
+                parentActivity.finish();
 
             }
         });
@@ -159,15 +186,18 @@ public class MonthCalendarActivity extends AppCompatActivity {
 
         // マニフェストファイルによって、activity_month_calendar.xmlとの紐付けができてるので、
         // idとかもこのまま使える メインレイアウトと同じだけど問題なく使える
-        RecyclerView rv = findViewById(R.id.rv);
+        RecyclerView rv = view.findViewById(R.id.rv);
         rv.setHasFixedSize(true);  // パフォーマンス向上
 
         // グリッド状にカードを配置する 7つづつ
-        GridLayoutManager manager = new GridLayoutManager(this, SPAN_COUNT);
+      //   GridLayoutManager manager = new GridLayoutManager(this, SPAN_COUNT);
+        GridLayoutManager manager = new GridLayoutManager(parentActivity, SPAN_COUNT);
         rv.setLayoutManager(manager);
 
         RecyclerView.Adapter adapter = new CalendarAdapter(data);
         rv.setAdapter(adapter);
+
+        return view;
 
     }
 }
