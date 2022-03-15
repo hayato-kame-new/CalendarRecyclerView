956c647df58836b9391f16fa847a37a438f31487
hayato-kame-new <guishan.meisha0330@gmail.com>
2022-03-15 18:47:35 +0900

    time_schedule_list_item.xml

---
+++ app/src/main/java/to/msn/wings/calendarrecyclerview/TimeScheduleListItem.java
@@ -0,0 +1,61 @@
+package to.msn.wings.calendarrecyclerview;
+
+public class TimeScheduleListItem {
+
+    private long id = 0;
+    private String date = null;
+    private String startTime = null;
+    private String endTime = null;
+    private String scheduleTitle = null;
+    private String scheduleMemo = null;
+
+    // アクセッサ ゲッター
+    public long getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public String getScheduleTitle() {
+        return scheduleTitle;
+    }
+
+    public String getScheduleMemo() {
+        return scheduleMemo;
+    }
+
+    // セッター
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setScheduleTitle(String scheduleTitle) {
+        this.scheduleTitle = scheduleTitle;
+    }
+
+    public void setScheduleMemo(String scheduleMemo) {
+        this.scheduleMemo = scheduleMemo;
+    }
+}
